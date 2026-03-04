acddd97bb9df13fa95548cfc67ca0b82639f4c9f
moonlighttttluna <danicamaevinar10@gmail.com>
2026-03-04 16:18:12 +0800

    input updated

--- processors/calculate_column_median.py
+++ processors/calculate_column_median.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def calculate_column_median(csv_file):
     df = pd.read_csv(csv_file)
-    numeric_df = df.select_dtypes(include=['number'])
+    numeric_df = df.select_dtypes(include='number')
     return numeric_df.median()
--- processors/generate_statistical_summary.py
+++ processors/generate_statistical_summary.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 def generate_statistical_summary(csv_file):
     df = pd.read_csv(csv_file)
-    return df.describe()
+    numeric_df = df.select_dtypes(include='number')
+    return numeric_df.describe()
