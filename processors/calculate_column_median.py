789071db2c0c3b53ffac2c8026679da7c7a1fafd
Vivince188 <vncyr18@gmail.com>
2026-03-24 17:13:45 +0800

    Update calculate_column_median.py

--- processors/calculate_column_median.py
+++ processors/calculate_column_median.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-def calculate_column_median(csv_file):
-    df = pd.read_csv(csv_file)
+def calculate_column_median(df):
     numeric_df = df.select_dtypes(include='number')
     return numeric_df.median()
