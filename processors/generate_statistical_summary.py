9a609a5c23dd719f004ef9a29dcc1c7b6fe5d65b
Vivince188 <vncyr18@gmail.com>
2026-03-24 17:14:37 +0800

    Update generate_statistical_summary.py

--- processors/generate_statistical_summary.py
+++ processors/generate_statistical_summary.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-def generate_statistical_summary(csv_file):
-    df = pd.read_csv(csv_file)
+def generate_statistical_summary(df):
     numeric_df = df.select_dtypes(include='number')
     return numeric_df.describe()
