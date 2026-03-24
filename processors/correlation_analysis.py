343fce526e0142813d41b02d2f2095dbc3a0a038
Vivince188 <vncyr18@gmail.com>
2026-03-24 17:14:51 +0800

    Update correlation_analysis.py

--- processors/correlation_analysis.py
+++ processors/correlation_analysis.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-def correlation_analysis(csv_file):
-    df = pd.read_csv(csv_file)
-    numeric_df = df.select_dtypes(include=['number'])
+def correlation_analysis(df):
+    numeric_df = df.select_dtypes(include='number')
     return numeric_df.corr()
